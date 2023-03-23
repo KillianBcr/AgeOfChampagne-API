dee1efa5693fd4cd4f9e90b3f1343d8edc17a9db
Killian <killianboscher@gmail.com>
2023-03-23 15:23:02 +0100

    modification du fichier CarteFixtures

--- src/DataFixtures/CarteFixtures.php
+++ src/DataFixtures/CarteFixtures.php
@@ -2,16 +2,14 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CarteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class CarteFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        CarteFactory::createMany(20);
     }
 }
