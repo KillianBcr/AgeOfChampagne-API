7317331936fc54904fbeb4e20e72fcf63c66e4b8
levi0009 <lohan.levis@etudiant.univ-reims.fr>
2023-03-24 10:57:02 +0100

    ajout des premiers tests

--- tests/Api/Entity/TestPartenaire/PartenairePutCest.php
+++ tests/Api/Entity/TestPartenaire/PartenairePutCest.php
@@ -2,7 +2,37 @@
 
 namespace App\Tests\Api\Entity\TestPartenaire;
 
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
 class PartenairePutCest
 {
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'nom' => 'string',
+            'email' => 'string:email',
+            'telephone' => 'string',
+            'description' => 'string',
+            'image'=> 'blob',
+        ];
+    }
+
+    public function anonymousUserForbiddenToPutUser(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        PartenaireFactory::createOne();
+
+        // 2. 'Act'
+        $I->sendPut('/api/users/1');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
+    }
+
+
+
+
 
 }
