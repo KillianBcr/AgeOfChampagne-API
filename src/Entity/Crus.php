706574453b28b7594343ba3cce3d0d922f8514fe
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2023-03-27 18:04:21 +0200

    Modification de l'Entité Crus (Ajout des méthode...)

--- src/Entity/Crus.php
+++ src/Entity/Crus.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CrusRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CrusRepository::class)]
@@ -13,8 +15,93 @@ class Crus
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 30)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'crus')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Cepage $cepage = null;
+
+    #[ORM\OneToMany(mappedBy: 'crus', targetEntity: Carte::class)]
+    private Collection $cartes;
+
+    public function __construct()
+    {
+        $this->cartes = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCepage(): ?Cepage
+    {
+        return $this->cepage;
+    }
+
+    public function setCepage(?Cepage $cepage): self
+    {
+        $this->cepage = $cepage;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Carte>
+     */
+    public function getCartes(): Collection
+    {
+        return $this->cartes;
+    }
+
+    public function addCarte(Carte $carte): self
+    {
+        if (!$this->cartes->contains($carte)) {
+            $this->cartes->add($carte);
+            $carte->setCrus($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCarte(Carte $carte): self
+    {
+        if ($this->cartes->removeElement($carte)) {
+            // set the owning side to null (unless already changed)
+            if ($carte->getCrus() === $this) {
+                $carte->setCrus(null);
+            }
+        }
+
+        return $this;
+    }
+
 }
