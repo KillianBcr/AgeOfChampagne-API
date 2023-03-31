d9473b39d6f26bbdda2ec8f274fe4818b221f7f5
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2023-03-31 09:49:23 +0200

    Changement du fichier Collection

--- src/Entity/Collection.php
+++ src/Entity/Collection.php
@@ -13,8 +13,38 @@ class Collection
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column]
+    private ?int $userID = null;
+
+    #[ORM\Column]
+    private ?int $cardID = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUserID(): ?int
+    {
+        return $this->userID;
+    }
+
+    public function setUserID(int $userID): self
+    {
+        $this->userID = $userID;
+
+        return $this;
+    }
+
+    public function getCardID(): ?int
+    {
+        return $this->cardID;
+    }
+
+    public function setCardID(int $cardID): self
+    {
+        $this->cardID = $cardID;
+
+        return $this;
     }
 }
