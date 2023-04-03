8422cebc108445d68d137eb8e81cffe951a9fc90
Luc <luc.schoeny@etudiant.univ-reims.fr>
2023-04-03 21:28:10 +0200

    Ajout des imports api dans l'entity region

--- src/Entity/Region.php
+++ src/Entity/Region.php
@@ -2,9 +2,17 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\RegionRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+
 
 #[ORM\Entity(repositoryClass: RegionRepository::class)]
 class Region
