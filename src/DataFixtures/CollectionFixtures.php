16c754de5d9ce127f74f34e82260b1dbf3711d47
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2023-04-04 09:20:21 +0200

    Changement de CollectionFixtures

--- src/DataFixtures/CollectionFixtures.php
+++ src/DataFixtures/CollectionFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Collection;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
-class CollectionFixtures extends Fixture
+class CollectionFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager): void
+    private $faker;
+
+    public function __construct()
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->faker = Factory::create();
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 20; ++$i) {
+            $collection = new Collection();
+            $collection->setUserID(1);
+            $collection->setCardID($i+1);
+            $manager->persist($collection);
+        }
 
         $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class,
+        );
     }
 }
