69b675173d31f388006ed9cac78aba0a8e7b08d7
Luc <luc.schoeny@etudiant.univ-reims.fr>
2023-04-05 20:26:11 +0200

    Creation du fichier test Region

---
+++ tests/Api/Entity/TestRegion/RegionPutCest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Api\Entity\TestRegion;
+
+use App\Entity\User;
+use App\Factory\PartenaireFactory;
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
+class RegionPutCest
+{
+
+}
