a144db7595d861a8803f902995b02de902510c07
Luc <luc.schoeny@etudiant.univ-reims.fr>
2023-04-05 20:27:40 +0200

    Test de la création d'une instance de Région

--- tests/Api/Entity/TestRegion/RegionPutCest.php
+++ tests/Api/Entity/TestRegion/RegionPutCest.php
@@ -3,12 +3,17 @@
 namespace App\Tests\Api\Entity\TestRegion;
 
 use App\Entity\User;
-use App\Factory\PartenaireFactory;
+use App\Factory\RegionFactory;
 use App\Factory\UserFactory;
 use App\Tests\Support\ApiTester;
 use Codeception\Util\HttpCode;
 
 class RegionPutCest
 {
+    public function testCreateRegion(): void
+{
+    $region = new Region();
 
+    $this->assertInstanceOf(Region::class, $region);
+}
 }
