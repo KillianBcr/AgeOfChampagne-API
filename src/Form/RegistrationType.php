21a1f2340616671c5243cf3f664246df4bbfb6ed
Samina Angelo <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-04-06 14:51:49 +0200

    Création d'un formulaire d'inscription

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('cp')
+            ->add('ville')
+            ->add('datenais')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
