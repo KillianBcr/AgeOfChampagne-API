d1d9a6b2da4755732196f0ef3429ac99a4bd54b0
Samina Angelo <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-04-06 15:03:38 +0200

    Modification des champs

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -4,26 +4,161 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToArrayTransformer;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type;
+
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('created_at')
-            ->add('updated_at')
-            ->add('cp')
-            ->add('ville')
-            ->add('datenais')
-        ;
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '180',
+                ],
+                'label' => 'Email',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4',
+                ],
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 180]),
+                ],
+            ])
+
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '30',
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4',
+                ],
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 30]),
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '30',
+                ],
+                'label' => 'Prenom',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4',
+                ],
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 30]),
+                ],
+            ])
+
+            ->add('datenais', Type\DateType::class, [
+                'input'=>'datetime',
+                'label' => 'Date de naissance',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4'
+                ],
+                'widget' => 'single_text'
+            ])
+
+            ->add('telephone', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '30',
+                ],
+                'label' => 'Telephone',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4',
+                ],
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 30]),
+                ],
+            ])
+
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '30',
+                ],
+                'label' => 'Ville',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4',
+                ],
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 30]),
+                ],
+            ])
+
+            ->add('cp', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '5',
+                    'maxlenght' => '6',
+                ],
+                'label' => 'Code postale',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4',
+                ],
+                'constraints' => [
+                    new Length(['min' => 5, 'max' => 6]),
+                ],
+            ])
+
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                    'label' => 'Mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label  mt-4',
+                    ],
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                    'label' => 'Confirmation du mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label  mt-4',
+                    ],
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+            ])
+            /*
+             ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            */
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-lg btn-primary',
+                ],
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
